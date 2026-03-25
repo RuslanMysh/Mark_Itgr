7c1d0098ae3f743828de133027bd8c430a67045f
SirJohn <jogernaut10@gmail.com>
2026-03-25 21:08:49 +0700

    Гравитация существует

--- Assets/Scripts/Mark/GameInput.cs
+++ Assets/Scripts/Mark/GameInput.cs
@@ -8,14 +8,21 @@ public class GameInput : MonoBehaviour
     private bool isRunning = false;
     public bool IsRunning => isRunning;
     //.............................................................
+    private bool isJump = false;
+    public bool IsJump => isJump;
+
+
 
     private void Awake()
     {
         inputActions = new PlayerInputActions();
         inputActions.Player.Enable();
 
-       
 
+        //.............................................................
+        inputActions.Player.Jump.performed += context => isJump = true;
+        inputActions.Player.Jump.canceled += context => isJump = false;
+        //.............................................................
         //.............................................................
         inputActions.Player.Sprint.performed += context => isRunning = true;
         inputActions.Player.Sprint.canceled += context => isRunning = false;
